940a3b8d2c842ed5435576ea0cd076047766fe27
LiuLinhong <liulinhong-b@rongcloud.cn>
2021-04-07 17:22:18 +0800

    Init

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '9.0'
+
+source 'https://github.com/CocoaPods/Specs.git'
+
+target 'RongRTCZoomVideoView' do
+	
+  pod 'RongRTCLib', '5.1.0'
+	pod 'Masonry'
+end
---
+++ RongRTCZoomVideoView/Supporting Files/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RongRTCZoomVideoView
+//
+//  Created by LiuLinhong on 2021/04/07.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
